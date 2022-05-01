ac58a9d3ae13413e3245a27df38895ce6fb906ed
Evgen <Deribasovich@gmail.com>
2022-05-01 15:11:06 +0300

    add functions

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt('How many films yve seen?', 0);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many films yve seen?', 0);
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films yve seen?', 0);
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,25 +18,43 @@ const personalMovieDB = {
     privat: false
 };
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('One of the latest films', '');
+        const b = prompt('Оценка', '');
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('One of the latest films', '');
-    const b = prompt('Оценка', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+};
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов')
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель')
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман!')
     } else {
-        i--;
+        console.log("Ошибка");
     }
 }
-console.log(personalMovieDB);
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов')
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель')
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман!')
-} else {
-    console.log("Ошибка");
+detectPersonalLevel();
+
+function showMyDB() {
+    return personalMovieDB.privat === false ? console.log(personalMovieDB) : console.log('no rights to see');
 }
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`, ''));
+    }
+}
+writeYourGenres();
+showMyDB();
