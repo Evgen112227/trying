456bc8540a3a725a99746d9db181dbaacbd99f6b
Evgen <Deribasovich@gmail.com>
2022-05-01 14:09:25 +0300

    added for cycle

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('One of the latest films', '');
-const b = prompt('Оценка', '');
-const c = prompt('One of the latest films', '');
-const d = prompt('Оценка', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('One of the latest films', '');
+    const b = prompt('Оценка', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!')
+} else {
+    console.log("Ошибка");
+}
